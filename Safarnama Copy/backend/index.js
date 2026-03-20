23645cb4b2d0d55e9042705a6537de87ddcdb486
Sakshi <sakshi9317@gmail.com>
2026-03-20 14:42:06 +0530

    update json and origin issues

--- Safarnama Copy/backend/index.js
+++ Safarnama Copy/backend/index.js
@@ -45,7 +45,7 @@ mongoose.connection
 const app = express();
 app.use(express.json());
 app.use(cors({ 
-  origin: ["https://traveldiary-flame.vercel.app", "http://localhost:5173"],
+  origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
